2fe3190f06e6ba209854d90085d761821894bb7c
Andy Cornforth <andycornforth@live.co.uk>
2016-07-06 18:31:31 +0100

    Initial commit. Included is the web api client library provided and an empty shopping list api with a test project.

---
+++ ShoppingListAPI.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoppingListAPI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
