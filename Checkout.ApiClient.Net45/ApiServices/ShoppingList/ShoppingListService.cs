a5b2737e768c553df3d3e6b29092e6c81718e5fb
Andy Cornforth <andycornforth@live.co.uk>
2016-07-08 20:32:07 +0100

    Written method descriptions for all methods in the client to match pattern of other services.

--- Checkout.ApiClient.Net45/ApiServices/ShoppingList/ShoppingListService.cs
+++ Checkout.ApiClient.Net45/ApiServices/ShoppingList/ShoppingListService.cs
@@ -6,56 +6,68 @@ namespace Checkout.ApiServices.ShoppingList
 {
     public class ShoppingListService
     {
-
-        //public HttpResponse<Item> GetItem(string itemName)
-        //{
-        //    return new ApiHttpClient().GetRequest<Item>(ApiUrls.ReportingTransactions, AppSettings.SecretKey);
-        //}
-
+        /// <summary>
+        /// Add an item to the list
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public HttpResponse<OkResponse> AddItem(Product item)
         {
             var uri = $"{ApiUrls.ShoppingList}/Post";
             return new ApiHttpClient().PostRequest<OkResponse>(uri, AppSettings.SecretKey, item);
         }
 
+        /// <summary>
+        /// Clear the entire list of items
+        /// </summary>
+        /// <returns></returns>
         public HttpResponse<OkResponse> ClearList()
         {
             var uri = $"{ApiUrls.ShoppingList}/ClearList";
             return new ApiHttpClient().GetRequest<OkResponse>(uri, AppSettings.SecretKey);
         }
 
+        /// <summary>
+        /// Request an item from the list by name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public HttpResponse<Product> GetItem(string name)
         {
             var uri = $"{ApiUrls.ShoppingList}/Get/{name}";
             return new ApiHttpClient().GetRequest<Product>(uri, AppSettings.SecretKey);
         }
 
+        /// <summary>
+        /// Delete an item from the list by name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public HttpResponse<OkResponse> DeleteItem(string name)
         {
             var uri = $"{ApiUrls.ShoppingList}/Delete/{name}";
             return new ApiHttpClient().DeleteRequest<OkResponse>(uri, AppSettings.SecretKey);
         }
 
+        /// <summary>
+        /// Update the quantity of an item by name
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public HttpResponse<OkResponse> UpdateItem(Product item)
         {
             var uri = $"{ApiUrls.ShoppingList}/Put";
             return new ApiHttpClient().PutRequest<OkResponse>(uri, AppSettings.SecretKey, item);
         }
 
+        /// <summary>
+        /// Request the full list of items to be returned
+        /// </summary>
+        /// <returns></returns>
         public HttpResponse<List<Product>> GetList()
         {
             var uri = $"{ApiUrls.ShoppingList}/GetList";
             return new ApiHttpClient().GetRequest<List<Product>>(uri, AppSettings.SecretKey);
         }
-
-        ///// <summary>
-        ///// Search for a customer’s chargebacks by a date range and then drill down using further filters.
-        ///// </summary>
-        ///// <param name="requestModel"></param>
-        ///// <returns></returns>
-        //public HttpResponse<QueryChargebackResponse> QueryChargeback(QueryRequest requestModel)
-        //{
-        //    return new ApiHttpClient().PostRequest<QueryChargebackResponse>(ApiUrls.ReportingChargebacks, AppSettings.SecretKey, requestModel);
-        //}
     }
 }
