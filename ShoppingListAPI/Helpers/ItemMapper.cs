49604fcb1923b082e0f43a50f0f7f5bb783e08bf
Andy Cornforth <andycornforth@live.co.uk>
2016-07-11 15:31:30 +0100

    Commit of Item Mapper. Unsure why it wasn't previously included.

---
+++ ShoppingListAPI/Helpers/ItemMapper.cs
@@ -0,0 +1,42 @@
+﻿using ShoppingListAPI.Models;
+using System.Collections.Generic;
+
+namespace ShoppingListAPI.Helpers
+{
+    public static class ItemMapper
+    {
+        public static Repository.Models.Item APIToRepository(Item item)
+        {
+            return new Repository.Models.Item()
+            {
+                Name = item.Name,
+                Quantity = item.Quantity
+            };
+        }
+
+        public static Item RepositoryToAPI(Repository.Models.Item item)
+        {
+            return new Item()
+            {
+                Name = item.Name,
+                Quantity = item.Quantity
+            };
+        }
+
+        public static List<Item> RepositoryListToAPIList(IList<Repository.Models.Item> list)
+        {
+            var apiList = new List<Item>();
+
+            foreach (var item in list)
+            {
+                apiList.Add(new Item()
+                {
+                    Name = item.Name,
+                    Quantity = item.Quantity
+                });
+            }
+
+            return apiList;
+        }
+    }
+}
